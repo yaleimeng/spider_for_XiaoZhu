30c46fd4159aadc014c18e12a11c48a93d1af624
Yalei Meng <yaleimeng@sina.com>
2017-09-05 09:34:45 +0800

    在open文件时指定编码。encoding='utf-8'

--- smallPig.py
+++ smallPig.py
@@ -10,7 +10,6 @@ from bs4 import BeautifulSoup
 import requests as rq
 import time
 import csv
-import codecs
 
   #从首页出发。目标是分析300个链接。根据数字规律构造网址的列表。
 site =['http://gz.xiaozhu.com/search-duanzufang-p{}-0/'.format(str(i)) for i in range(1,15)]
@@ -69,8 +68,7 @@ for eve in newUrls:         #针对newUrls里面每个url,做详情页的爬取�
 #把词典数据写入到csv文件。
 print('字典列表的个数为%d'%len(csvRows))
 rowHeader = ['title','address','roomPic','price','owner','gender','ownerPic']
-with open('E:/romm.csv','w')as f:
-    f.write(codecs.BOM_UTF8)
+with open('E:/romm.csv','w',encoding='utf-8')as f:
     f_csv = csv.DictWriter(f,rowHeader)
     f_csv.writeheader()
     f_csv.writerows(csvRows)
